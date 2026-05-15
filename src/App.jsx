42364ee5121f6878fd77fd1ac2371e4e4e03fceb
densinela <densinela@gmail.com>
2026-05-15 10:21:15 +0300

    Updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-<Route path="/login" element={<Login />} />
+
 <Route path="/Dashboard" element={<Dashboard/>} />
 <Route path="/community" element={<Community />} />
 <Route path="/signup" element={<Signup />} />
